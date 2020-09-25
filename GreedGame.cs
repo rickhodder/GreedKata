063e0ec1a090194ec022f1e17bd6e5f25f6e6708
rickhodder <rickhodder@msn.com>
2020-09-25 17:37:30 -0400

    Contains 5 should return 50

--- GreedGame.cs
+++ GreedGame.cs
@@ -1,9 +1,21 @@
+using System.Linq;
+
 namespace GreedKata
 {
     public class GreedGame
     {
         public int Score(Roll roll)
         {
+            if (roll.Dice.Any(d=>d==1))
+            {
+                return 100;
+            }
+
+            if (roll.Dice.Any(d=>d==5))
+            {
+                return 50;
+            }
+
             return 0;
         }
     }
--- GreedGameTests.cs
+++ GreedGameTests.cs
@@ -5,12 +5,34 @@ namespace GreedKata
 {
     public class GreedGameTests
     {
+        private GreedGame _game;
+        private Roll _roll;
+
+        public GreedGameTests()
+        {
+            _game = new GreedGame();
+            _roll = new Roll(); 
+        }
+
         [Fact]
         public void Score_ShouldReturnZero()
-        {
-            var game = new GreedGame();  
-            var roll = new Roll();                      
-            Assert.Equal(0,game.Score(roll));
+        {   
+            Assert.Equal(0,_game.Score(_roll));            
         }
+
+        [Fact]
+        public void Score_ContainsOne_ShouldReturn100()
+        {
+            _roll.Dice =new int[] {1,2,3,4,5};
+            Assert.Equal(100,_game.Score(_roll));            
+        }
+
+        [Fact]
+        public void Score_ContainsFive_ShouldReturn50()
+        {
+            _roll.Dice =new int[] {2,2,3,4,5};
+            Assert.Equal(50,_game.Score(_roll));            
+        }
+
     }
 }
